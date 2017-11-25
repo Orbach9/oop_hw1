7de6e2b7599b6788046db7a9c13fcd584533dea8
Orbach <orbaci@emc.com>
2017-11-25 19:35:45 +0200

    ggg
    Signed-off-by: Orbach <orbaci@emc.com>

--- src/homework1/LocationChangingOval.java
+++ src/homework1/LocationChangingOval.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.Color;
 
 public class LocationChangingOval extends LocationChangingShape {
 	
@@ -16,16 +17,21 @@ public class LocationChangingOval extends LocationChangingShape {
 		bounds.setLocation(location);
 	}
 	
-	public void setSize(Dimension dimension) {
-		
+	public void setSize(Dimension dimension) throws ImpossibleSizeException {
+		if (dimension.getHeight()<0 || dimension.getWidth()<0) {
+			ImpossibleSizeException ex = new ImpossibleSizeException(dimension);
+			throw ex;
+		}
+		bounds.setSize(dimension);
 	}
 	
 	public Rectangle getBounds() {
-		
+		return bounds;
 	}
 	
 	public void draw(Graphics g) {
-		
+		g.setColor(this.getColor());
+		g.fillOval((int) this.getLocation().getX(),(int) this.getLocation().getY()
+				,(int) bounds.getWidth(),(int) bounds.getHeight());
 	}
-
 }
